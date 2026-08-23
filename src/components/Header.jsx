7b087dbaf3e39ca96a2a96b27933fb010673aedd
joshua <pierson.joshua726@gmail.com>
2026-08-23 15:30:41 -0500

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,30 +3,29 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { Link } from "react-router-dom";
-import NavDropdown from "react-bootstrap/NavDropdown";
-import HomePage from "./HomePage";
 
 export default function Header() {
   return (
-    <div>
-      <Navbar expand="lg" className="bg-body-tertiary">
-        <Container>
-          <Navbar.Brand className="sriracha-regular" as={Link} to="/">
-            To-Do List Maker
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link className="sriracha-regular" as={Link} to="/todo">
-                To-Do List Page
-              </Nav.Link>
-              <Nav.Link className="sriracha-regular" as={Link} to="/contact">
-                Contact Page
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </div>
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand className="sriracha-regular" as={Link} to="/">
+          To-Do List Maker
+        </Navbar.Brand>
+
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link className="sriracha-regular" as={Link} to="/todo">
+              To-Do List Page
+            </Nav.Link>
+
+            <Nav.Link className="sriracha-regular" as={Link} to="/contact">
+              Contact Page
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
