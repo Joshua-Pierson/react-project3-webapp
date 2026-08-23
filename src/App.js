11634b48156d531e941d910719b681d2f944c464
joshua <pierson.joshua726@gmail.com>
2026-08-23 14:47:39 -0500

    update header

--- src/App.js
+++ src/App.js
@@ -1,38 +1,54 @@
-import React, {useState} from 'react';
-import './App.css'; 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import HomePage from './components/HomePage';
-import Header from './components/Header';
-import ToDoList from './components/ToDoList';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Navigate } from 'react-router-dom';
-
-
-
+import React, { useState } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import HomePage from "./components/HomePage";
+import Header from "./components/Header";
+import ToDoList from "./components/ToDoList";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 
 function App() {
-  
   const [todolist, setToDoList] = React.useState([
-    { id: 1, task: 'Buy groceries', status: 'Not Completed', completed:' ', delete:  ' ' },
-    { id: 2, task: 'Walk the dog', status: 'Not Completed', completed:' ', delete:  ' ' },
-    { id: 3, task: 'Read a book', status: 'Not Completed', completed:' ', delete:  ' ' },
+    {
+      id: 1,
+      task: "Buy groceries",
+      status: "Not Completed",
+      completed: " ",
+      delete: " ",
+    },
+    {
+      id: 2,
+      task: "Walk the dog",
+      status: "Not Completed",
+      completed: " ",
+      delete: " ",
+    },
+    {
+      id: 3,
+      task: "Read a book",
+      status: "Not Completed",
+      completed: " ",
+      delete: " ",
+    },
   ]);
 
-  
   return (
-    <div >
-    <Router>
+    <div>
       <Header />
-      <Routes>
-        <Route path="/homepage" element={<HomePage />} />
-        <Route path="/todo" element={<ToDoList todolist={todolist} setToDoList={setToDoList}  />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/" element={<HomePage />} />
-      </Routes>
-      <Footer />
-    </Router>
+      <Router>
+        <Routes>
+          <Route path="/homepage" element={<HomePage />} />
+          <Route
+            path="/todo"
+            element={<ToDoList todolist={todolist} setToDoList={setToDoList} />}
+          />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
